167bc17534cd22a04791a725dba849a709d59438
Hilai <hilai301209@gmail.com>
2026-09-23 19:45:43 -0300

    Ajustanto coisas

--- MeuDiarioSenac.WebApi/Program.cs
+++ MeuDiarioSenac.WebApi/Program.cs
@@ -1,7 +1,14 @@
+using System.Text.Json.Serialization;
 using MeuDiarioSenac.Model;
 using MeuDiarioSenac.Service;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "boa noite");
@@ -9,8 +16,7 @@ app.MapGet("/", () => "boa noite");
 app.MapGet("/motivacional", () => "Não desista, grandes coisas levam tempo. Continue persistindo e você alcançará seus objetivos!");
 
 app.MapGet("/registros", () => {
-    List<Registro> registros = new RegistroService().ListarRegistros();
-    return registros;
+    return new RegistroService().ListarTodos();
 });
 
 
