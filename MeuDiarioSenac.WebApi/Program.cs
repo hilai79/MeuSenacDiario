c63a6d50a858bbfe4b8413f43cb849a0bdf6671e
Hilai <hilai301209@gmail.com>
2026-09-23 21:46:35 -0300

    adicionado os 4 endpoints

--- MeuDiarioSenac.WebApi/Program.cs
+++ MeuDiarioSenac.WebApi/Program.cs
@@ -1,8 +1,10 @@
 using System.Text.Json.Serialization;
 using MeuDiarioSenac.Model;
 using MeuDiarioSenac.Service;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
@@ -11,12 +13,43 @@ builder.Services.ConfigureHttpJsonOptions(options =>
 
 var app = builder.Build();
 
+var registrosGroup = app.MapGroup("/registros");
+
 app.MapGet("/", () => "boa noite");
 
 app.MapGet("/motivacional", () => "Não desista, grandes coisas levam tempo. Continue persistindo e você alcançará seus objetivos!");
 
-app.MapGet("/registros", () => {
-    return new RegistroService().ListarTodos();
+registrosGroup.MapGet("/", () => {
+    return new RegistroService().ListarRegistros();
+});
+
+registrosGroup.MapPost("/", ([FromBody] Registro registro) => {
+    new RegistroService().CriarRegistro(
+        registro.Titulo,
+        registro.Conteudo,
+        registro.Usuario.Nome);
+
+    return "Registro adicionado com sucesso!";
+});
+
+registrosGroup.MapDelete("/", () => {
+    var registrosApagados = new RegistroService().ApagarTodosRegistros();
+    return Results.Ok($"{registrosApagados} registro(s) excluído(s) com sucesso!");
+});
+
+registrosGroup.MapPut("/{id:int}", (int id, [FromBody] Registro registro) => {
+    var registroAtualizado = new RegistroService().EditarRegistro(
+        id,
+        registro.Titulo,
+        registro.Conteudo,
+        registro.Usuario.Nome);
+
+    if (registroAtualizado is null)
+    {
+        return Results.NotFound($"Registro com id {id} não encontrado.");
+    }
+
+    return Results.Ok(registroAtualizado);
 });
 
 
